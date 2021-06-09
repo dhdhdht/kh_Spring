a3e9d18efe872b28990b56a27bfe9bab1f7ff966
jin <breakboy0526@gmail.com>
2021-06-09 23:36:27 +0900

    Spring DI01

---
+++ Spring01_Hello/test02/MessageBeanEn.java
@@ -0,0 +1,13 @@
+package com.test02;
+
+public class MessageBeanEn implements MessageBean {
+
+	@Override
+	public void sayHello(String name) {
+		System.out.println("Hello, " + name);
+		
+	}
+	
+	
+
+}
