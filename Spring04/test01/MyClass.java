f4008bb6416e40234ad318a22c662dae0b6fa30b
dhdhdht <breakboy0526@naver.com>
2021-06-18 22:05:30 +0900

    spring

---
+++ Spring04/test01/MyClass.java
@@ -0,0 +1,13 @@
+package com.test01;
+
+public class MyClass {
+	
+	public MyClass() {
+		System.out.println("MyClass Constructor!!!");
+	}
+	
+	public void prn() {
+		System.out.println("MyClass.prn(); !!!!");
+	}
+
+}
